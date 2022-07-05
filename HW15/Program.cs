a04858589d08ac081b640934b35ad8d7482a0fac
jonsi <jonsi@DESKTOP-VI5FVR6>
2022-07-05 21:07:51 +0400

    Добавьте файлы проекта.

---
+++ HW15/Program.cs
@@ -0,0 +1,23 @@
+﻿var classes = new[]
+{
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+};
+
+var allStudents = GetAllStudents(classes);
+
+Console.WriteLine(string.Join(" ", allStudents));
+
+
+static string[] GetAllStudents(Classroom[] classes)
+{
+    var allStudentss = classes.SelectMany(x => x.Students).ToArray();
+
+    return allStudentss;
+}
+
+public class Classroom
+{
+    public List<string> Students = new List<string>();
+}
